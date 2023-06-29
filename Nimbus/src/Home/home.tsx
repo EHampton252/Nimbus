e2931fe1c4c4bcd67268bdebe41a6933c607165f
Ethan Hampton <ethanhampton00@gmail.com>
2023-06-28 21:17:55 -0400

    Init

---
+++ Nimbus/src/Home/home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <h2>Todays Weather</h2>
+            <h2>5 Day Forecast</h2>
+            <h2>Weekly Forecast</h2>
+        </div>
+    )
+}
