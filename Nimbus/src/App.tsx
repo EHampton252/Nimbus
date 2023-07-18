dea40f4edb61d98dfed3c678b07c9a6d15819719
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-17 22:25:54 -0400

    WIP Error on Side menu

--- Nimbus/src/App.tsx
+++ Nimbus/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './Pages/Home/home.js'
 import LocationsPage from './Pages/Locations/locations.js'
 import SettingsPage from './Pages/Settings/settings-page.js'
