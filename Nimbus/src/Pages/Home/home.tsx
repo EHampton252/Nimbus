e69e4bd047d90e67b3c5aa3929d1b2f6fe261114
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-22 15:08:25 -0400

    init grid container

--- Nimbus/src/Pages/Home/home.tsx
+++ Nimbus/src/Pages/Home/home.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import SideMenu from './components/side-menu'
 
 export default function Home() {
     return (
         <>
-        <div>
+        <div className="home-page">
             <h1>Home</h1>
+            <div className="weather-card basic-weather"></div>
             <h2>Todays Weather</h2>
             <h2>5 Day Forecast</h2>
             <h2>Weekly Forecast</h2>
