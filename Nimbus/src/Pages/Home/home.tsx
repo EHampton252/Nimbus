c26737cca75bdb6ceaec94b8033a7be2838e8d52
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-22 22:23:14 -0400

    WIP Grid layout

--- Nimbus/src/Pages/Home/home.tsx
+++ Nimbus/src/Pages/Home/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import './home.css'
+
 export default function Home() {
     return (
         <>
         <div className="home-page">
             <h1>Home</h1>
-            <div className="weather-card basic-weather"></div>
             <h2>Todays Weather</h2>
+            <div className="weather-card basic-weather"></div>
             <h2>5 Day Forecast</h2>
             <h2>Weekly Forecast</h2>
         </div>
