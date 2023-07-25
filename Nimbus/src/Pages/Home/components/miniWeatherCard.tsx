39a928b7b6cfb40bf29fe8da9367bc47c5df16c0
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-24 20:37:33 -0400

    init minicards

---
+++ Nimbus/src/Pages/Home/components/miniWeatherCard.tsx
@@ -0,0 +1,10 @@
+import 'home.css'
+import React from 'react'
+
+const MiniWeatherCard = () => {
+    return (
+        <div className="weather-card mini-card">Basic weather</div>
+    )
+}
+
+export default MiniWeatherCard;
