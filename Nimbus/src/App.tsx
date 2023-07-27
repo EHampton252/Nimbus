295d8ea2b8721bb50f4aab0a10c1d0740ce2b412
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-26 22:19:30 -0400

    Saving the API call for later

--- Nimbus/src/App.tsx
+++ Nimbus/src/App.tsx
@@ -23,3 +23,6 @@ function App() {
 }
 
 export default App
+
+
+// https://api.open-meteo.com/v1/forecast?latitude=39.9612&longitude=-82.9988&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,windspeed_10m,windspeed_80m,windgusts_10m,temperature_80m,uv_index,is_day&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York
