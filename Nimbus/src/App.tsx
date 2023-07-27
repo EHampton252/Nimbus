c3d960adc69a4c9f7aa58c8e0cef5742b5021190
Ethan Hampton <ethanhampton00@gmail.com>
2023-07-26 21:23:42 -0400

    Page organization and cleanup

--- Nimbus/src/App.tsx
+++ Nimbus/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './Pages/Home/home.js'
-import LocationsPage from './Pages/Locations/locations.js'
 import SettingsPage from './Pages/Settings/settings-page.js'
 import './App.css'
 import SideMenu from './Pages/Home/components/side-menu.js'
@@ -15,7 +14,6 @@ function App() {
         <div className="page-content-container">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/locations" element={<LocationsPage />} />
             <Route path="/settings" element={<SettingsPage />} />
           </Routes>
         </div>
